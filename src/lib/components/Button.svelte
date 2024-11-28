555c2a55d8cba9c59c3484c40e49173c93382a5d
jacobmcmichael <jacobmcmichael@gmail.com>
2024-11-28 00:52:27 -0800

    :feat: adds button component with style import

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { SvelteComponent } from "svelte";
+	import "$lib/styles/button.css";
+
+	interface ButtonProps {
+		type?: "submit" | "reset" | "button";
+		icon?: SvelteComponent;
+		value?: string;
+		variant?: "primary" | "secondary";
+		customClass?: string;
+	}
+
+	let {
+		type = "button",
+		icon,
+		value = "button",
+		variant = "primary",
+		customClass,
+	}: ButtonProps = $props();
+</script>
+
+<button
+	class={`button--${variant} ${customClass}`.trim()}
+	{type}
+>
+	{icon}
+	<span>{value}</span>
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+	import Button from "$lib/components/Button.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+	documentation
+</p>
+<Button />
